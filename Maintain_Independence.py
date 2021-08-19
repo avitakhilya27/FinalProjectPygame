15fb3790a295f0fc204e227aa6e1023c81b41e5e
avitakhilya27 <89155682+avitakhilya27@users.noreply.github.com>
2021-08-19 22:28:50 +0700

    Add files via upload

--- Maintain_Independence.py
+++ Maintain_Independence.py
@@ -1,3 +1,4 @@
+#import library pygame
 import pygame
 ## pengaturan tampilan game
 # mengatur ukuran layar
@@ -27,8 +28,10 @@ clock = pygame.time.Clock()
 
 #penerapan object oriented progamming
 class Game:
+    #frame rate yang digunakan 60 fame per detik
     TICK_RATE = 60
 
+    #inisialisasi class untuk set up title , width, heigth
     def __init__(self, image_path, title, width, height):
         self.title = title
         self.width = width
@@ -36,9 +39,10 @@ class Game:
 
         # menampilkan screen
         self.game_screen = pygame.display.set_mode((width, height))
+        #warna putih pada backgraound
         self.game_screen.fill(WHITE_COLOR)
         pygame.display.set_caption(title)
-
+        #load dan set background image game
         background_image = pygame.image.load(image_path)
         self.image = pygame.transform.scale(background_image, (width, height))
 
@@ -46,11 +50,14 @@ class Game:
         is_game_over = False
         direction = 0
         did_win = False
+
+        #memutar sound saat game mulai
         start_sound.play()
 
         player_character = PlayerCharacter('asset/player.png', 275, 550, 50, 50)
 
         enemy_1 = EnemyCharacter('asset/musuh1.png', 20, 450, 50, 50)
+        #kecepatan musush bertambah ketika mencapai bendera
         enemy_1.SPEED += level_speed
 
         enemy_2 = EnemyCharacter('asset/musuh2.png', self.width - 40, 300, 50, 50)
@@ -63,37 +70,46 @@ class Game:
         enemy_4.SPEED += level_speed
 
         Flag = GameObject('asset/flag.png', 275, -15, 60, 60)
-        game_font = pygame.font.Font('04B_19.ttf', 20)
 
         # perulangan untuk mengupdate game
+        # Main game loop, digunakan untuk update semua gameplay seperti movement, checks, dan graphic
+        # Berjalan sampai is_game_over = True
         while not is_game_over:
             for event in pygame.event.get():
+                # ketika kita menekan tombol keluar (esc), maka akan keluar dari game loop
                 if event.type == pygame.QUIT:
                     is_game_over = True
+                # Deteksi ketika menekan panah turun
                 elif event.type == pygame.KEYDOWN:
+                    # Player bergerak ke atas
                     if event.key == pygame.K_UP:
                         direction = 1
                         langkah_sound.play()
+                    # ketikan arah/ tombol dilepas
                     elif event.key == pygame.K_DOWN:
                         direction = -1
                         langkah_sound.play()
+                #ketika menekan tombol keatas
                 elif event.type == pygame.KEYUP:
                     if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                        #gerakan player berhenti
                         direction = 0
                         langkah_sound.play()
                 print(event)
 
-            # memanggil karakter
+            # memampilkan screen
             self.game_screen.fill(WHITE_COLOR)
             self.game_screen.blit(self.image,(0, 0))
 
-            player_character.move(direction, self.height)
+            #menampilkan karakter(player, enemy)
+            player_character.move(direction)
             player_character.draw(self.game_screen)
             Flag.draw(self.game_screen)
 
             enemy_1.move(self.width)
             enemy_1.draw(self.game_screen)
 
+            #move dan draw enemy ketika kecepatannya makin bertambah
             if level_speed > 2 :
                 enemy_2.move(self.width)
                 enemy_2.draw(self.game_screen)
@@ -104,7 +120,7 @@ class Game:
                 enemy_4.move(self.width)
                 enemy_4.draw(self.game_screen)
 
-            #jika bertabrakan
+            #jika bertabrakan dengan enemy, game berhenti
             if player_character.detection_collison(enemy_1):
                 is_game_over = True
                 did_win = False
@@ -137,6 +153,8 @@ class Game:
                 pygame.display.update()
                 clock.tick(1)
                 break
+
+            #jika bertabrakan dengan benderaa, game berlanjut
             if player_character.detection_collison(Flag):
                 is_game_over = True
                 did_win = True
@@ -148,13 +166,12 @@ class Game:
 
             pygame.display.update()
             clock.tick(self.TICK_RATE)
-
-        if did_win: #jika menang, game akan berulang, jika kalah, game berakhir
+        if did_win: #jika menang, game akan berulang dengan kecepatan bertambah 1, jika kalah game berakhir
             self.run_game_loop(level_speed + 1.0)
         else:
             return
 
-
+#Class objek game generik untuk disubklasifikasikan oleh objek lain dalam game
 class GameObject:
     def __init__(self, image_path, x, y, width, height):
         self.x_pos = x
@@ -170,20 +187,20 @@ class GameObject:
     def draw(self, background):
         background.blit(self.image, (self.x_pos, self.y_pos)) #blit untuk menambahkan karakter objek
 
+#class yang mewakili karakter pemain
 class PlayerCharacter(GameObject):
 
     SPEED = 5
 
     def __init__(self, image_path, x, y, width, height):
+        #subclass yang mewarisi (inharitance) dari superclasss game object
         super().__init__(image_path, x, y, width, height)
 
-    def move(self, direction, max_height):
+    def move(self, direction):
         if direction > 0:
             self.y_pos -= self.SPEED
         elif direction < 0 :
             self.y_pos += self.SPEED
-        if self.y_pos >= max_height - 50:
-            self.y_pos = max_height - 50
 
     def detection_collison(self, other_body):
         if self.y_pos > other_body.y_pos + other_body.height: #jika posisi pemain diatas enemy, game berlanjut
@@ -194,12 +211,11 @@ class PlayerCharacter(GameObject):
             return False
         elif self.x_pos + self.width < other_body.x_pos: #jika posisi pemain dikiri enemy, game lanjut
             return False
-
         return True
 
-
+#Kelas untuk mewakili musuh yang bergerak dari kiri ke kanan dan kanan ke kiri
 class EnemyCharacter(GameObject):
-
+    #kecepatan musuh
     SPEED = 5
 
     def __init__(self, image_path, x, y, width, height):
